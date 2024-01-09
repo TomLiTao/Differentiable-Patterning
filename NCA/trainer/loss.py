c4afde3eb5c03516f26c3cd3a97a085f7052beca
Alex <alexander.richardson@gmail.com>
2024-01-09 12:03:04 +0000

    added vgg loss

--- NCA/trainer/loss.py
+++ NCA/trainer/loss.py
@@ -1,5 +1,10 @@
 import jax.numpy as jnp
 import jax
+#from eqxvision.models import alexnet
+#from eqxvision.utils import CLASSIFICATION_URLS
+import equinox as eqx
+from lpips_j.lpips import LPIPS
+#import eqxvision as eqv
 """
 	General format of loss functions here:
 
@@ -16,6 +21,10 @@ import jax
 		loss reduced over channel and spatial axes
 
 """
+#loaded_alexnet = alexnet(torch_weights=CLASSIFICATION_URLS['alexnet'])
+#loaded_vgg11 = eqv.models.vgg11(torch_weights=CLASSIFICATION_URLS["vgg11"])
+lpips = LPIPS()
+
 @jax.jit
 def l2(x,y):
 
@@ -31,3 +40,62 @@ def random_sampled_euclidean(x,y,key,SAMPLES=64):
 	x_sub = jax.random.choice(key,x_r.reshape((-1,x_r.shape[-1])),(SAMPLES,),False)
 	y_sub = jax.random.choice(key,y_r.reshape((-1,y_r.shape[-1])),(SAMPLES,),False)
 	return jnp.sqrt(jnp.mean((x_sub-y_sub)**2,axis=0))
+
+
+# @eqx.filter_jit
+# def alexnet(x,y, key):
+# 	"""
+# 	NOTE THAT CHANNELS IS TRUNCATED TO 3
+
+# 	Parameters
+# 	----------
+# 	x : float32 [N,CHANNELS,WIDTH,HEIGHT]
+# 		predictions
+# 	y : float32 [N,CHANNELS,WIDTH,HEIGHT]
+# 		true data
+# 	key : jax.random.PRNGKey
+# 		Jax random number key. 
+
+# 	Returns
+# 	-------
+# 	loss : float32 [N]
+
+# 	"""
+# 	x = jnp.einsum("ncxy->nxyc",x)[...,:3]
+# 	y = jnp.einsum("ncxy->nxyc",y)[...,:3]
+# 	
+# 	lpips = LPIPS(net="alex")
+# 	
+# 	params = lpips.init(key, x, y)
+# 	loss = lpips.apply(params, x, y)
+# 	return loss
+	
+        
+@eqx.filter_jit
+def vgg(x,y, key):
+	"""
+	NOTE THAT CHANNELS IS TRUNCATED TO 3
+
+	Parameters
+	----------
+	x : float32 [N,CHANNELS,WIDTH,HEIGHT]
+		predictions
+	y : float32 [N,CHANNELS,WIDTH,HEIGHT]
+		true data
+	key : jax.random.PRNGKey
+		Jax random number key. 
+
+	Returns
+	-------
+	loss : float32 [N]
+
+	"""
+	x = jnp.einsum("ncxy->nxyc",x)[...,:3]
+	y = jnp.einsum("ncxy->nxyc",y)[...,:3]
+	
+	
+	
+	params = lpips.init(key, x, y)
+	loss = lpips.apply(params, x, y)
+	return loss
+	
