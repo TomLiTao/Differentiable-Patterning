074d99fdce614c5e2989f3e8d1ab3c5cf3d9249a
Alex <alexander.richardson@gmail.com>
2024-01-09 14:02:07 +0000

    fixing noise augmentation

--- NCA/utils.py
+++ NCA/utils.py
@@ -106,7 +106,7 @@ def key_pytree_gen(key,shape):
 	key_array : uint32[shape,2]
 		array of random keys
 	"""
-	shape = list(shape)
+	shape = [shape]
 	shape.append(2)
 	key_array = jax.random.randint(key,shape=shape,minval=0,maxval=2_147_483_647,dtype="uint32")
 	key_array = list(key_array)
