36620b152fa767705385f2451b2cbd893f36153e
Alex <alexander.richardson@gmail.com>
2024-01-09 12:05:12 +0000

    defined agent neural networks

---
+++ ABM/model/agent_nn_angle.py
@@ -0,0 +1,87 @@
+import jax
+import equinox as eqx
+import jax.numpy as jnp
+import time
+
+class agent_nn(eqx.Module):
+	layers: list
+	layers_pheremone: list
+	layers_velocity_mag: list
+	layers_d_angle: list
+	N_CHANNELS: int
+	
+	def __init__(self,N_CHANNELS,key=jax.random.PRNGKey(int(time.time()))):
+		key1,key2,key3,key4,key5 = jax.random.split(key,5)
+		self.N_CHANNELS=N_CHANNELS
+		def flat_func(x):
+			return jnp.array(x).flatten()
+		self.layers = [flat_func,
+				       eqx.nn.Linear(in_features=3*self.N_CHANNELS,	# Reads local pheremone concentrations and combines them together
+							         out_features=self.N_CHANNELS,
+									 use_bias=True,
+									 key=key1),
+				       jax.nn.relu
+# 					   eqx.nn.Linear(in_features=self.N_CHANNELS,	# Reads local pheremone concentrations and combines them together
+# 							         out_features=self.N_CHANNELS,
+# 									 use_bias=True,
+# 									 key=key2),
+# 					   jax.nn.relu
+					   ]
+		self.layers_pheremone = [ 
+					   eqx.nn.Linear(in_features=self.N_CHANNELS, 	# Updates the pheremone concentrations at agent position
+							         out_features=self.N_CHANNELS,
+									 use_bias=True,
+									 key=key3),
+					   jnp.tanh 									# Bounds updates of pheremone between 1 and -1
+					   ]	
+		self.layers_velocity_mag = [									
+					   eqx.nn.Linear(in_features=self.N_CHANNELS, 	# Updates agent velocity 
+							         out_features=1,
+									 use_bias=False,
+									 key=key4),
+					   jax.nn.hard_tanh 									# Keeps agent speed non negative
+					   ]
+		self.layers_d_angle = [									
+					   eqx.nn.Linear(in_features=self.N_CHANNELS, 	# Updates agent velocity 
+							         out_features=1,
+									 use_bias=False,
+									 key=key5),
+					   jax.nn.hard_tanh									# Keeps agent speed non negative
+					   ]
+# 		w_where = lambda l: l.weight
+# 		self.layers_pheremone[0] = eqx.tree_at(w_where,
+# 											   self.layers_pheremone[0],
+# 											   jax.random.uniform(key=key1,
+# 																  shape=self.layers_pheremone[0].weight.shape,
+# 																  minval=-1,
+# 																  maxval=1)
+# 											   )
+# 		self.layers_velocity_mag[0] = eqx.tree_at(w_where,
+# 											   self.layers_velocity_mag[0],
+# 											   jax.random.uniform(key=key2,
+# 																  shape=self.layers_velocity_mag[0].weight.shape,
+# 																  minval=-1,
+# 																  maxval=1)
+# 											   )
+# 		self.layers_d_angle[0] = eqx.tree_at(w_where,
+# 											   self.layers_d_angle[0],
+# 											   jax.random.uniform(key=key2,
+# 																  shape=self.layers_d_angle[0].weight.shape,
+# 																  minval=-1,
+# 																  maxval=1)
+# 											   )
+	@eqx.filter_jit
+	def __call__(self,X):
+		for L in self.layers:
+			X = L(X)
+		v = X
+		dp= X
+		da= X
+		for L in self.layers_pheremone:
+			dp = L(dp)
+		for L in self.layers_velocity_mag:
+			v = L(v)
+		for L in self.layers_d_angle:
+			da = L(da)
+		return v,dp,da
+		
---
+++ ABM/model/agent_nn_vel.py
@@ -0,0 +1,69 @@
+import jax
+import equinox as eqx
+import jax.numpy as jnp
+import time
+
+class agent_nn(eqx.Module):
+	layers: list
+	layers_pheremone: list
+	layers_velocity: list
+	N_CHANNELS: int
+	
+	def __init__(self,N_CHANNELS,key=jax.random.PRNGKey(int(time.time()))):
+		key1,key2,key3,key4 = jax.random.split(key,4)
+		self.N_CHANNELS=N_CHANNELS
+		def flat_func(x):
+			return jnp.array(x).flatten()
+		self.layers = [flat_func,
+				       eqx.nn.Linear(in_features=3*self.N_CHANNELS,	# Reads local pheremone concentrations and combines them together
+							         out_features=self.N_CHANNELS,
+									 use_bias=True,
+									 key=key1),
+				       jax.nn.relu
+# 					   eqx.nn.Linear(in_features=self.N_CHANNELS,	# Reads local pheremone concentrations and combines them together
+# 							         out_features=self.N_CHANNELS,
+# 									 use_bias=True,
+# 									 key=key2),
+# 					   jax.nn.relu
+					   ]
+		self.layers_pheremone = [ 
+					   eqx.nn.Linear(in_features=self.N_CHANNELS, 	# Updates the pheremone concentrations at agent position
+							         out_features=self.N_CHANNELS,
+									 use_bias=True,
+									 key=key3),
+					   jnp.tanh 									# Bounds updates of pheremone between 1 and -1
+					   ]	
+		self.layers_velocity = [									
+					   eqx.nn.Linear(in_features=self.N_CHANNELS, 	# Updates agent velocity 
+							         out_features=2,
+									 use_bias=False,
+									 key=key4),
+					   jnp.tanh 									# Bounds agent velocity components between 1 and -1
+					   ]
+		w_where = lambda l: l.weight
+		self.layers_pheremone[0] = eqx.tree_at(w_where,
+											   self.layers_pheremone[0],
+											   jax.random.uniform(key=key1,
+																  shape=self.layers_pheremone[0].weight.shape,
+																  minval=-1,
+																  maxval=1)
+											   )
+		self.layers_velocity[0] = eqx.tree_at(w_where,
+											   self.layers_velocity[0],
+											   jax.random.uniform(key=key2,
+																  shape=self.layers_velocity[0].weight.shape,
+																  minval=-1,
+																  maxval=1)
+											   )
+	@eqx.filter_jit
+	def __call__(self,X):
+		for L in self.layers:
+			X = L(X)
+		v = X
+		dp= X
+		for L in self.layers_pheremone:
+			dp = L(dp)
+		for L in self.layers_velocity:
+			v = L(v)
+		return v,dp
+		
