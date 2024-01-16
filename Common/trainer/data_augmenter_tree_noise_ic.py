94cafde10ce94ce16a7cafa07835b758a3339089
Alex <alexander.richardson@gmail.com>
2024-01-16 14:26:06 +0000

    proper parallelising over random keys

--- Common/trainer/data_augmenter_tree_noise_ic.py
+++ Common/trainer/data_augmenter_tree_noise_ic.py
@@ -14,7 +14,7 @@ class DataAugmenterNoise(DataAugmenterAbstract):
         data = self.duplicate_batches(data, 4)
         key = jax.random.PRNGKey(int(time.time()))
         #keys = jax.random.split(key,len(data))
-        keys = key_pytree_gen(key,(len(data)))
+        keys = key_pytree_gen(key,[len(data),])
         set_x0_noise = lambda x,key:x.at[0].set(jax.random.uniform(key,shape=x[0].shape,minval=0,maxval=1))	
         data = jax.tree_util.tree_map(set_x0_noise,data,keys)
         self.save_data(data)
