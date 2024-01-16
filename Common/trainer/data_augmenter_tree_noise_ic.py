962cc09b22616fba91b5ca2e136bf04de2a4f0d3
Alex <alexander.richardson@gmail.com>
2024-01-16 12:06:45 +0000

    noise ic texture learning

---
+++ Common/trainer/data_augmenter_tree_noise_ic.py
@@ -0,0 +1,52 @@
+from Common.trainer.abstract_data_augmenter_tree import DataAugmenterAbstract
+import jax
+import time
+class DataAugmenterNoise(DataAugmenterAbstract):
+    """Sets initial condition x[0] to noise at each timestep, and randomly subsample target images
+
+    Args:
+        DataAugmenterAbstract (_type_): _description_
+    """
+    def data_init(self):
+        data = self.return_saved_data()
+        data = self.duplicate_batches(data, 4)		
+        self.save_data(data)
+        return None
+    
+    def data_callback(self, x, y, i):
+        """
+		Called after every training iteration to perform data augmentation and processing		
+
+		Parameters
+		----------
+		x : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
+			Initial conditions
+		y : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
+			Final states
+		i : int
+			Current training iteration - useful for scheduling mid-training data augmentation
+
+		Returns
+		-------
+		x : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
+			Initial conditions
+		y : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
+			Final states
+
+		"""
+        if hasattr(self, "PREVIOUS_KEY"):
+            key = jax.random.fold_in(self.PREVIOUS_KEY,i)
+        else:
+            key = jax.random.PRNGKey(int(time.time()))
+            self.PREVIOUS_KEY = key
+        x_true,_ =self.split_x_y(1)
+        propagate_xn = lambda x:x.at[1:].set(x[:-1])
+        set_x0_noise = lambda x:x.at[0].set(jax.random.uniform(key,shape=x[0].shape,minval=0,maxval=1))	
+        x = jax.tree_util.tree_map(propagate_xn,x) # Set initial condition at each X[n] at next iteration to be final state from X[n-1] of this iteration
+        x = jax.tree_util.tree_map(set_x0_noise,x) 
+        if i < 500:		
+            for b in range(len(x)//2):
+                x[b*2] = x[b*2].at[1:,:self.OBS_CHANNELS].set(x_true[b*2][1:,:self.OBS_CHANNELS]) # Set every other batch of intermediate initial conditions to correct initial conditions
+        key = jax.random.foldin(key,i)
+        x = self.noise(x,0.005,key=key) # Add little noise to next steps for stability
+        return x,y
--- texture_nca.py
+++ texture_nca.py
@@ -1,64 +1,16 @@
 from NCA.model.NCA_model import NCA
 from NCA.trainer.NCA_trainer import NCA_Trainer
 from Common.utils import load_textures
-from Common.trainer.abstract_data_augmenter_tree import DataAugmenterAbstract
-import jax
+from Common.trainer.data_augmenter_tree_noise_ic import DataAugmenterNoise
+
 import time
 
-class DataAugmenterCustom(DataAugmenterAbstract):
-	def data_init(self,SHARDING = None):
-		"""
-		Chain together various data augmentations to perform at intialisation of NCA training
-
-		"""
-		data = self.return_saved_data()
-		data = self.duplicate_batches(data, 4)		
-		self.save_data(data)
-		return None
-
-	def data_callback(self,x,y,i):
-		"""
-		Called after every training iteration to perform data augmentation and processing		
-
-		Parameters
-		----------
-		x : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
-			Initial conditions
-		y : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
-			Final states
-		i : int
-			Current training iteration - useful for scheduling mid-training data augmentation
-
-		Returns
-		-------
-		x : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
-			Initial conditions
-		y : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
-			Final states
-
-		"""
-		x_true,_ =self.split_x_y(1)
-		propagate_xn = lambda x:x.at[1:].set(x[:-1])
-		reset_x0 = lambda x,x_true:x.at[0].set(x_true[0])	
-		x = jax.tree_util.tree_map(propagate_xn,x) # Set initial condition at each X[n] at next iteration to be final state from X[n-1] of this iteration
-		x = jax.tree_util.tree_map(reset_x0,x,x_true) # Keep first initial x correct
-		if i < 500:		
-			for b in range(len(x)//2):
-				x[b*2] = x[b*2].at[:,:self.OBS_CHANNELS].set(x_true[b*2][:,:self.OBS_CHANNELS]) # Set every other batch of intermediate initial conditions to correct initial conditions
-		if hasattr(self, "PREVIOUS_KEY"):
-			key = jax.random.fold_in(self.PREVIOUS_KEY,i)
-		else:
-			key=jax.random.PRNGKey(int(time.time()))
-		x = self.noise(x,0.005,key=key)
-		self.PREVIOUS_KEY = key
-		return x,y
-		
 
 CHANNELS=16
 
 #data = load_emoji_sequence(["crab.png","alien_monster.png","alien_monster.png"],downsample=2)
-data = load_textures(["dotted/dotted_0109.jpg","braided/braided_0075.jpg","braided/braided_0135.jpg"],downsample=2,crop_square=True)
-t=64
+data = load_textures(["dotted/dotted_0109.jpg","braided/braided_0075.jpg","braided/braided_0075.jpg"],downsample=4,crop_square=True)
+t=32
 iters=2000
 
 
@@ -67,8 +19,8 @@ nca = NCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],FIRE_RATE=0.5,PERIODIC=True)
 opt = NCA_Trainer(nca,
 				  data,
 				  #model_filename="micropattern_radii_sized_b"+str(B)+"_r1e-2_v2_"+str(index),
-				  model_filename="emoji_texture_nca_test_8",
-				  DATA_AUGMENTER=DataAugmenterCustom)
+				  model_filename="emoji_texture_nca_test_9",
+				  DATA_AUGMENTER=DataAugmenterNoise)
 				  #BOUNDARY_MASK=masks,
 				    
 
