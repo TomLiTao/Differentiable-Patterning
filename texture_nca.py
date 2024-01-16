850b07a9b2574a4b6d298552c200e864c1340f40
Alex <alexander.richardson@gmail.com>
2024-01-16 15:33:29 +0000

    texture changes

--- texture_nca.py
+++ texture_nca.py
@@ -9,7 +9,7 @@ import time
 CHANNELS=16
 
 #data = load_emoji_sequence(["crab.png","alien_monster.png","alien_monster.png"],downsample=2)
-data = load_textures(["dotted/dotted_0109.jpg","honeycombed/honeycombed_0059.jpg","interlaced/interlaced_0093.jpg"],downsample=4,crop_square=True)
+data = load_textures(["dotted/dotted_0109.jpg","honeycombed/honeycombed_0059.jpg","veined/veined_0095.jpg"],downsample=4,crop_square=True)
 t=64
 iters=2000
 
@@ -19,7 +19,7 @@ nca = NCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],FIRE_RATE=0.5,PERIODIC=True)
 opt = NCA_Trainer(nca,
 				  data,
 				  #model_filename="micropattern_radii_sized_b"+str(B)+"_r1e-2_v2_"+str(index),
-				  model_filename="emoji_texture_nca_test_13",
+				  model_filename="emoji_texture_nca_test_14",
 				  DATA_AUGMENTER=DataAugmenterNoise)
 				  #BOUNDARY_MASK=masks,
 				    
