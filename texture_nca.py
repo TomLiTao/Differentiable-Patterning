3c1501ddd932429cfcb3fba6ec79c93d07609edd
Alex <alexander.richardson@gmail.com>
2024-01-17 11:35:29 +0000

    changing resolution and channels

--- texture_nca.py
+++ texture_nca.py
@@ -6,11 +6,11 @@ from Common.trainer.data_augmenter_tree_noise_ic import DataAugmenterNoise
 import time
 
 
-CHANNELS=32
+CHANNELS=16
 
 #data = load_emoji_sequence(["crab.png","alien_monster.png","alien_monster.png"],downsample=2)
 data = load_textures(["dotted/dotted_0109.jpg","honeycombed/honeycombed_0059.jpg","veined/veined_0095.jpg"],downsample=2,crop_square=True,crop_factor=2)
-t=32
+t=64
 iters=2000
 
 
@@ -19,7 +19,7 @@ nca = NCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],FIRE_RATE=0.5,PERIODIC=True)
 opt = NCA_Trainer(nca,
 				  data,
 				  #model_filename="micropattern_radii_sized_b"+str(B)+"_r1e-2_v2_"+str(index),
-				  model_filename="emoji_texture_nca_test_16",
+				  model_filename="emoji_texture_nca_test_17",
 				  DATA_AUGMENTER=DataAugmenterNoise)
 				  #BOUNDARY_MASK=masks,
 				    
