8e2b37241decb8adbdb8069980340a7ef3f8540b
Alex <alexander.richardson@gmail.com>
2024-01-19 12:18:58 +0000

    refactored tensorboard logging

---
+++ Common/trainer/abstract_tensorboard_log.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import numpy as np
+
+
+
+
+
+
+class Train_log(object):
+
+
+    def __init__(self,log_dir,data,RGB_mode="RGB"):
+        """
+			Initialises the tensorboard logging of training.
+			Writes some initial information. Very similar to setup_tb_log_single, but designed for sequence modelling
+
+		"""
+
+        self.LOG_DIR = log_dir
+        self.RGB_mode = RGB_mode
+
+
+        train_summary_writer = tf.summary.create_file_writer(self.LOG_DIR)
+
+        #--- Log the target image and initial condtions
+        with train_summary_writer.as_default():
+            for b in range(len(data)):
+                if self.RGB_mode=="RGB":
+                    #tf.summary.image('True sequence RGB',np.einsum("ncxy->nxyc",data[0,:,:3,...]),step=0,max_outputs=data.shape[0])
+                    tf.summary.image('True sequence RGB',np.einsum("ncxy->nxyc",data[b][:,:3,...]),step=b,max_outputs=data[b].shape[0])
+                elif self.RGB_mode=="RGBA":
+                    #tf.summary.image('True sequence RGBA',np.einsum("ncxy->nxyc",data[0,:,:4,...]),step=0,max_outputs=data.shape[0])
+                    tf.summary.image('True sequence RGBA',np.einsum("ncxy->nxyc",data[b][:,:4,...]),step=b,max_outputs=data[b].shape[0])
+            
+        self.train_summary_writer = train_summary_writer
+
+    def tb_training_loop_log_sequence(self,losses,x,i,model,write_images=True):
+
+
+        BATCHES = losses.shape[0]
+        N = losses.shape[1]
+        with self.train_summary_writer.as_default():
+            tf.summary.histogram("Loss",losses,step=i)
+            tf.summary.scalar("Average Loss",np.mean(losses),step=i)
+            for n in range(N):
+                tf.summary.histogram("Loss of each batch, timestep "+str(n),losses[:,n],step=i)
+                tf.summary.scalar("Loss of averaged over each batch, timestep "+str(n),np.mean(losses[:,n]),step=i)
+            for b in range(BATCHES):
+                tf.summary.histogram("Loss of each timestep, batch "+str(b),losses[b],step=i)
+                tf.summary.scalar("Loss of averaged over each timestep,  batch "+str(b),np.mean(losses[b]),step=i)
+
+        if i%10==0:
+            self.log_model_parameters(model,i)
+            if write_images:
+                self.log_model_outputs(x,i)
+
+    def tb_training_end_log(self,model,x,t,*args):
+        raise NotImplementedError
+    
+    def log_model_parameters(self,model,i):
+        raise NotImplementedError
+    
+    def log_model_outputs(self,x,i):
+        raise NotImplementedError
