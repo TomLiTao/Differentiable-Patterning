814fc6bc844fea5ca0b7a0121ad903f2bfd14910
Alex <alexander.richardson@gmail.com>
2024-02-08 17:10:23 +0000

    rerunning with correct loss function

--- Experiments/training_explore.array.sh
+++ Experiments/training_explore.array.sh
@@ -4,6 +4,6 @@
 #$ -cwd
 #$ -l h_rt=24:00:00
 
-#$ -q gpu -l gpu=1 -pe sharedmem 4 -l h_vmem=64G
+#$ -q gpu -l gpu=1 -pe sharedmem 4 -l h_vmem=80G
 export CUDA_VISIBLE_DEVICES=$SGE_HGR_gpu
 bash training_explore.sh $SGE_TASK_ID
--- nca_emoji_channels_sampling.py
+++ nca_emoji_channels_sampling.py
@@ -16,16 +16,16 @@ key = jr.PRNGKey(int(time.time()))
 key = jr.fold_in(key,index)
 key_model,key_trainer = jr.split(key,2)
 N_BATCHES = 4
-TRAIN_ITERS = 8000
-LEARN_RATE = 1e-3
+TRAIN_ITERS = 4000
+LEARN_RATE = 1e-2
 N_CHANNELS,SAMPLING = index_to_channel_sample(index)
 FILENAME = "model_exploration/emoji_"+str(N_CHANNELS)+"_channels_"+str(SAMPLING)+"_sampling_v1"
 
 
-schedule = optax.exponential_decay(1e-2, transition_steps=TRAIN_ITERS, decay_rate=0.99)
+schedule = optax.exponential_decay(LEARN_RATE, transition_steps=TRAIN_ITERS, decay_rate=0.99)
 optimiser = optax.adamw(schedule)
 
-data = load_emoji_sequence(["alien_monster.png","microbe.png","rooster_1f413.png","rooster_1f413.png"],downsample=2)
+data = load_emoji_sequence(["alien_monster.png","microbe.png","rooster_1f413.png","rooster_1f413.png"],downsample=1)
 
 nca = NCA(N_CHANNELS=N_CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],PERIODIC=False,key=key_model)
 opt = NCA_Trainer(nca,data,model_filename=FILENAME)
