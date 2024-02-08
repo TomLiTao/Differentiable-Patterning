31a4d5ba663a6763aab8348b843c4881b958562c
Alex <alexander.richardson@gmail.com>
2024-02-08 18:10:03 +0000

    running with different activations and kernels

---
+++ Experiments/training_kernel_activation_sweep.array.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+#$ -N kernel_activation_sweep
+#$ -M s1605376@ed.ac.uk
+#$ -cwd
+#$ -l h_rt=24:00:00
+
+#$ -q gpu -l gpu=1 -pe sharedmem 4 -l h_vmem=80G
+export CUDA_VISIBLE_DEVICES=$SGE_HGR_gpu
+bash training_kernel_activation_sweep.sh $SGE_TASK_ID
