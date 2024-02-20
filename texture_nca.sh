c55eff2179e6530c71ef03e286f398e2e22dd857
Alex <alexander.richardson@gmail.com>
2024-02-20 22:16:34 +0000

    adding bash script for texture nca

---
+++ texture_nca.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+#$ -N texture_nca
+#$ -M s1605376@ed.ac.uk
+#$ -cwd
+#$ -l h_rt=24:00:00
+
+#$ -q gpu -l gpu=1 -pe sharedmem 4 -l h_vmem=80G
+
+
+. /etc/profile.d/modules.sh
+
+export CUDA_VISIBLE_DEVICES=$SGE_HGR_gpu
+module load anaconda
+source activate jax_gpu
+
+python ./texture_nca.py 
+source deactivate
