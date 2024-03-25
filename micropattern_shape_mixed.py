2091e92bcd7ec1c393551b2c8b42fcb2ac8491a0
Alex <alexander.richardson@gmail.com>
2024-03-25 09:39:47 +0000

    fixed bug with incorrect adhesion masks being generated

--- micropattern_shape_mixed.py
+++ micropattern_shape_mixed.py
@@ -1,9 +1,10 @@
 from NCA.model.NCA_gated_model import gNCA
 from NCA.trainer.NCA_trainer import *
 from Common.utils import load_micropattern_radii,load_micropattern_ellipse,load_micropattern_triangle
-
+import matplotlib.pyplot as plt
 from NCA.NCA_visualiser import *
 import optax
+import einops
 import numpy as np
 import jax.numpy as jnp
 import jax
@@ -24,12 +25,15 @@ BATCHES = 1
 # Select which subset of data to train on
 #data,masks = load_micropattern_radii("../Data/micropattern_radii/Chir_Fgf_*")
 data_disc,masks_disc,_ = load_micropattern_radii("../Data//micropattern_shapes/Max Projections */lowres/*Disc*")
-data_triangle,masks_triangle,_ = load_micropattern_radii("../Data//micropattern_shapes/Max Projections */lowres/*Triangle*")
-data_ellipse,masks_ellipse,_ = load_micropattern_radii("../Data//micropattern_shapes/Max Projections */lowres/*Ellipse*")
+data_triangle,masks_triangle,_ = load_micropattern_triangle("../Data//micropattern_shapes/Max Projections */lowres/*Triangle*")
+data_ellipse,masks_ellipse,_ = load_micropattern_ellipse("../Data//micropattern_shapes/Max Projections */lowres/*Ellipse*")
 data = data_ellipse[index:index+1] + data_triangle[index:index+1] + data_disc[index:index+1]
 masks = masks_ellipse[index:index+1] + masks_triangle[index:index+1] + masks_disc[index:index+1]
 
-
+#plt.imshow(einops.rearrange(data[1][1][:3],"c x y -> x y c"))
+#plt.show()
+#plt.imshow(einops.rearrange(data[1][0][:3],"c x y -> x y c"))
+#plt.show()
 
 schedule = optax.exponential_decay(1e-2, transition_steps=iters, decay_rate=0.99)
 optimiser= optax.adamw(schedule)
@@ -54,5 +58,12 @@ opt = NCA_Trainer(nca,
 				  model_filename="micropattern_shapes_gated_mixed_no_norm"+str(index),
 				  BOUNDARY_MASK=masks,
 				  DATA_AUGMENTER = data_augmenter_subclass)
+x0,y0 = opt.DATA_AUGMENTER.data_load()
+print(len(x0))
+print(x0[0].shape)
+print(y0[0].shape)
+#plt.imshow(einops.rearrange(x0[0][0][:15],"(c w) x y -> (c x) y w",w=3))
+#plt.imshow(einops.rearrange(x0[1][0][:3],"c x y -> x y c"))
+#plt.show()
 
 opt.train(t,iters,optimiser=optimiser)
