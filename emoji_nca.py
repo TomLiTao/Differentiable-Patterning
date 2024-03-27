058d927a930299c1d88db9005de2e824f8583211
Alex <alexander.richardson@gmail.com>
2024-03-27 13:18:40 +0000

    replace sobel filters with gabor filters to try a smooth generalisation

--- emoji_nca.py
+++ emoji_nca.py
@@ -1,5 +1,6 @@
 #from NCA.model.NCA_model import NCA
 from NCA.model.NCA_gated_model import gNCA
+from NCA.model.NCA_smooth_model import cNCA
 from NCA.trainer.NCA_trainer import NCA_Trainer
 from Common.utils import load_emoji_sequence
 #from Common.trainer.data_augmenter_tree_noise_ic import DataAugmenterNoise
@@ -23,17 +24,17 @@ class data_augmenter_subclass(DataAugmenter):
         self.save_data(data)
         return None
 
-data = load_emoji_sequence(["crab.png","alien_monster.png","crab.png","alien_monster.png"],downsample=2)
+data = load_emoji_sequence(["crab.png","alien_monster.png","microbe.png","alien_monster.png"],downsample=2)
 #data = load_textures(["dotted/dotted_0109.jpg","honeycombed/honeycombed_0078.jpg","grid/grid_0002.jpg"],downsample=3,crop_square=True,crop_factor=1)
 schedule = optax.exponential_decay(1e-2, transition_steps=iters, decay_rate=0.99)
 optimiser = optax.chain(optax.scale_by_param_block_norm(),
                         optax.adamw(schedule))
 
-nca = gNCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],FIRE_RATE=0.5,PERIODIC=True)
+nca = cNCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],KERNEL_SCALE=3,FIRE_RATE=0.5,PERIODIC=True)
 print(nca)
 opt = NCA_Trainer(nca,
 				  data,
-				  model_filename="gate_emoji_nca_test_4",
+				  model_filename="emoji_smooth_nca_test_1",
 				  DATA_AUGMENTER=data_augmenter_subclass)
 				  
 				    
