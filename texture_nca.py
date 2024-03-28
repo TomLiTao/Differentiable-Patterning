017702b8bea442dd77463b7d00a869c03a635f6a
Alex <alexander.richardson@gmail.com>
2024-03-28 10:55:46 +0000

    testing smooth nca on high res texture synthesis

--- texture_nca.py
+++ texture_nca.py
@@ -4,7 +4,7 @@ from NCA.model.NCA_smooth_model import cNCA
 from NCA.trainer.NCA_trainer import NCA_Trainer
 from Common.utils import load_textures
 #from Common.trainer.data_augmenter_tree_noise_ic import DataAugmenterNoise
-from Common.trainer.data_augmenter_tree_subsample import DataAugmenterSubsampleNoiseTexture
+from Common.trainer.data_augmenter_tree_subsample_noise import DataAugmenterSubsampleNoiseTexture
 import time
 import optax
 
@@ -15,17 +15,24 @@ t=64
 iters=2000
 
 
-data = load_textures(["dotted/dotted_0109.jpg","honeycombed/honeycombed_0078.jpg","grid/grid_0002.jpg"],downsample=3,crop_square=True,crop_factor=1)
+data = load_textures(["dotted/dotted_0109.jpg","dotted/dotted_0109.jpg","honeycombed/honeycombed_0078.jpg","grid/grid_0002.jpg"],downsample=3,crop_square=True,crop_factor=1)
 schedule = optax.exponential_decay(1e-2, transition_steps=iters, decay_rate=0.99)
 optimiser = optax.chain(optax.scale_by_param_block_norm(),
                         optax.adamw(schedule))
 
 nca = cNCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],KERNEL_SCALE=3,FIRE_RATE=0.5,PERIODIC=True)
 print(nca)
+
+class da_subclass(DataAugmenterSubsampleNoiseTexture):
+    def __init__(self, data_true, hidden_channels=0):
+        super().__init__(data_true, hidden_channels)
+        self.sample_size = 64
+        self.resample_freq = 8
+
 opt = NCA_Trainer(nca,
 				  data,
-				  model_filename="texture_smooth_nca_test_1",
-				  DATA_AUGMENTER=DataAugmenterSubsampleNoiseTexture)
+				  model_filename="texture_smooth_highres_nca_test_1",
+				  DATA_AUGMENTER=da_subclass)
 				  
 				    
 
