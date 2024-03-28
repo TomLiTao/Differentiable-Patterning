7fd4c2440181b67416e9b7254f67cb47b03c2895
Alex <alexander.richardson@gmail.com>
2024-03-28 11:40:11 +0000

    testing gated smooth nca on high res image morphing

--- emoji_nca.py
+++ emoji_nca.py
@@ -1,6 +1,7 @@
 #from NCA.model.NCA_model import NCA
 from NCA.model.NCA_gated_model import gNCA
 from NCA.model.NCA_smooth_model import cNCA
+from NCA.model.NCA_smooth_gated_model import gcNCA
 from NCA.trainer.NCA_trainer import NCA_Trainer
 from Common.utils import load_emoji_sequence
 #from Common.trainer.data_augmenter_tree_noise_ic import DataAugmenterNoise
@@ -24,17 +25,17 @@ class data_augmenter_subclass(DataAugmenter):
         self.save_data(data)
         return None
 
-data = load_emoji_sequence(["crab.png","microbe.png","alien_monster.png","alien_monster.png"],downsample=2)
+data = load_emoji_sequence(["crab.png","microbe.png","alien_monster.png","alien_monster.png"],downsample=1)
 #data = load_textures(["dotted/dotted_0109.jpg","honeycombed/honeycombed_0078.jpg","grid/grid_0002.jpg"],downsample=3,crop_square=True,crop_factor=1)
 schedule = optax.exponential_decay(1e-2, transition_steps=iters, decay_rate=0.99)
 optimiser = optax.chain(optax.scale_by_param_block_norm(),
                         optax.adamw(schedule))
 
-nca = cNCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],KERNEL_SCALE=3,FIRE_RATE=0.5,PERIODIC=True)
+nca = gcNCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],KERNEL_SCALE=3,FIRE_RATE=0.5,PERIODIC=True)
 print(nca)
 opt = NCA_Trainer(nca,
 				  data,
-				  model_filename="emoji_smooth_nca_euclidean_test_2",
+				  model_filename="emoji_smooth_gated_nca_test_1",
 				  DATA_AUGMENTER=data_augmenter_subclass)
 				  
 				    
