b310e7b70b04d5cdea157f52829b51bfdebd5939
Alex <alexander.richardson@gmail.com>
2024-05-02 14:32:13 +0100

    different textures

--- texture_nca.py
+++ texture_nca.py
@@ -6,8 +6,8 @@ from NCA.trainer.NCA_trainer import NCA_Trainer
 import jax
 from Common.utils import load_textures
 #from Common.trainer.data_augmenter_tree_noise_ic import DataAugmenterNoise
-from Common.trainer.data_augmenter_tree_subsample_noise import DataAugmenterSubsampleNoiseTexture
-#from NCA.trainer.data_augmenter_nca import DataAugmenter
+#from Common.trainer.data_augmenter_tree_subsample_noise import DataAugmenterSubsampleNoiseTexture
+from NCA.trainer.data_augmenter_nca import DataAugmenter
 import time
 import optax
 
@@ -19,7 +19,7 @@ iters=2000
 
 
 #data = load_textures(["dotted/dotted_0109.jpg","dotted/dotted_0109.jpg","honeycombed/honeycombed_0078.jpg","grid/grid_0002.jpg"],downsample=3,crop_square=True,crop_factor=1)
-data = load_textures(["banded/banded_0109.jpg","dotted/dotted_0109.jpg","dotted/dotted_0109.jpg"],downsample=2,crop_square=True,crop_factor=1.5)
+data = load_textures(["banded/banded_0109.jpg","dotted/dotted_0109.jpg","honeycombed/honeycombed_0078.jpg","banded/banded_0109.jpg","dotted/dotted_0109.jpg"],downsample=2,crop_square=True,crop_factor=1.5)
 schedule = optax.exponential_decay(1e-2, transition_steps=iters, decay_rate=0.99)
 optimiser = optax.chain(optax.scale_by_param_block_norm(),
                         optax.nadam(schedule))
@@ -27,59 +27,59 @@ optimiser = optax.chain(optax.scale_by_param_block_norm(),
 nca = gNCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],FIRE_RATE=0.5,PERIODIC=True)
 print(nca)
 
-# class da_subclass(DataAugmenter):
-#     def data_init(self, SHARDING=None):
-#         data = self.return_saved_data()
-#         data = self.duplicate_batches(data, 4)
-#         self.save_data(data)
-#     def data_callback(self,x,y,i):
-#         """
-#         Called after every training iteration to perform data augmentation and processing		
+class da_subclass(DataAugmenter):
+    def data_init(self, SHARDING=None):
+        data = self.return_saved_data()
+        data = self.duplicate_batches(data, 4)
+        self.save_data(data)
+    def data_callback(self,x,y,i):
+        """
+        Called after every training iteration to perform data augmentation and processing		
 
 
-#         Parameters
-#         ----------
-#         x : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
-#             Initial conditions
-#         y : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
-#             Final states
-#         i : int
-#             Current training iteration - useful for scheduling mid-training data augmentation
+        Parameters
+        ----------
+        x : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
+            Initial conditions
+        y : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
+            Final states
+        i : int
+            Current training iteration - useful for scheduling mid-training data augmentation
 
-#         Returns
-#         -------
-#         x : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
-#             Initial conditions
-#         y : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
-#             Final states
+        Returns
+        -------
+        x : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
+            Initial conditions
+        y : PyTree [BATCHES] f32[N-N_steps,CHANNELS,WIDTH,HEIGHT]
+            Final states
 
-#         """
+        """
 
-#         x_true,_ =self.split_x_y(1)
-#         propagate_xn = lambda x:x.at[1:].set(x[:-1])
-#         reset_x0 = lambda x,x_true:x.at[0].set(x_true[0])
+        x_true,_ =self.split_x_y(1)
+        propagate_xn = lambda x:x.at[1:].set(x[:-1])
+        reset_x0 = lambda x,x_true:x.at[0].set(x_true[0])
         
-#         x = jax.tree_util.tree_map(propagate_xn,x) # Set initial condition at each X[n] at next iteration to be final state from X[n-1] of this iteration
-#         x = jax.tree_util.tree_map(reset_x0,x,x_true) # Keep first initial x correct
+        x = jax.tree_util.tree_map(propagate_xn,x) # Set initial condition at each X[n] at next iteration to be final state from X[n-1] of this iteration
+        x = jax.tree_util.tree_map(reset_x0,x,x_true) # Keep first initial x correct
         
-#         #if i < 500:		
-#         for b in range(len(x)//2):
-#             x[b*2] = x[b*2].at[:,:self.OBS_CHANNELS].set(x_true[b*2][:,:self.OBS_CHANNELS]) # Set every other batch of intermediate initial conditions to correct initial conditions
+        #if i < 500:		
+        for b in range(len(x)//2):
+            x[b*2] = x[b*2].at[:,:self.OBS_CHANNELS].set(x_true[b*2][:,:self.OBS_CHANNELS]) # Set every other batch of intermediate initial conditions to correct initial conditions
             
         
-#         if hasattr(self, "PREVIOUS_KEY"):
-#             key = jax.random.fold_in(self.PREVIOUS_KEY,i)
-#         else:
-#             key=jax.random.PRNGKey(int(time.time()))
+        if hasattr(self, "PREVIOUS_KEY"):
+            key = jax.random.fold_in(self.PREVIOUS_KEY,i)
+        else:
+            key=jax.random.PRNGKey(int(time.time()))
 
-#         x = self.noise(x,0.005,key=key)
-#         self.PREVIOUS_KEY = key
-#         return x,y
+        x = self.noise(x,0.005,key=key)
+        self.PREVIOUS_KEY = key
+        return x,y
 
 opt = NCA_Trainer(nca,
 				  data,
-				  model_filename="texture_gated_nca_test_7",
-				  DATA_AUGMENTER=DataAugmenterSubsampleNoiseTexture)
+				  model_filename="texture_gated_nca_test_8",
+				  DATA_AUGMENTER=da_subclass)
 				  
 				    
 
@@ -87,4 +87,5 @@ opt.train(t,
           iters,
           WARMUP=10,
           optimiser=optimiser,
-          LOSS_FUNC_STR="vgg")
+          LOSS_FUNC_STR="vgg",
+          LOOP_AUTODIFF="checkpointed")
