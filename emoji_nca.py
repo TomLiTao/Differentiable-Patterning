dbf6ee9c0d4b85fc928a69663de2043d434c4bb2
Alex <alexander.richardson@gmail.com>
2024-05-29 12:01:12 +0100

    training highres nca with zero_random_circles

--- emoji_nca.py
+++ emoji_nca.py
@@ -27,34 +27,34 @@ class data_augmenter_subclass(DataAugmenter):
     #Redefine how data is pre-processed before training
     def data_init(self,SHARDING=None):
         data = self.return_saved_data()
-        data = self.duplicate_batches(data, 2)
+        data = self.duplicate_batches(data, 4)
         data = self.pad(data, 10) 		
         self.save_data(data)
         return None
 
 if data_index == 0:
-    data = load_emoji_sequence(["crab.png","microbe.png","alien_monster.png"],downsample=DOWNSAMPLE)
+    data = load_emoji_sequence(["crab.png","microbe.png","alien_monster.png","alien_monster.png"],downsample=DOWNSAMPLE)
     data_filename = "cr_mi_al"
 if data_index == 1:
-    data = load_emoji_sequence(["microbe.png","avocado_1f951.png","alien_monster.png"],downsample=DOWNSAMPLE)
+    data = load_emoji_sequence(["microbe.png","avocado_1f951.png","alien_monster.png","alien_monster.png"],downsample=DOWNSAMPLE)
     data_filename = "mi_av_al"
 if data_index == 2:
-    data = load_emoji_sequence(["avocado_1f951.png","mushroom_1f344.png","lizard_1f98e.png"],downsample=DOWNSAMPLE)
+    data = load_emoji_sequence(["avocado_1f951.png","mushroom_1f344.png","lizard_1f98e.png","lizard_1f98e.png"],downsample=DOWNSAMPLE)
     data_filename = "av_mu_li"
 if data_index == 3:
-    data = load_emoji_sequence(["mushroom_1f344.png","alien_monster.png","rooster_1f413.png"],downsample=DOWNSAMPLE)
+    data = load_emoji_sequence(["mushroom_1f344.png","alien_monster.png","rooster_1f413.png","rooster_1f413.png"],downsample=DOWNSAMPLE)
     data_filename = "mu_al_ro"
 if data_index == 4:
-    data = load_emoji_sequence(["alien_monster.png","mushroom_1f344.png","alien_monster.png"],downsample=DOWNSAMPLE)
+    data = load_emoji_sequence(["alien_monster.png","mushroom_1f344.png","alien_monster.png","alien_monster.png"],downsample=DOWNSAMPLE)
     data_filename = "al_mu_al"
 if data_index == 5:
-    data = load_emoji_sequence(["butterfly.png","eye.png","lizard_1f98e.png"],downsample=DOWNSAMPLE)
+    data = load_emoji_sequence(["butterfly.png","eye.png","lizard_1f98e.png","lizard_1f98e.png"],downsample=DOWNSAMPLE)
     data_filename = "bu_ey_li"
 if data_index == 6:
-    data = load_emoji_sequence(["rooster_1f413.png","butterfly.png","eye.png"],downsample=DOWNSAMPLE)
+    data = load_emoji_sequence(["rooster_1f413.png","butterfly.png","eye.png","eye.png"],downsample=DOWNSAMPLE)
     data_filename = "ro_bu_ey"
 if data_index == 7:
-    data = load_emoji_sequence(["lizard_1f98e.png","microbe.png","anatomical_heart.png"],downsample=DOWNSAMPLE)
+    data = load_emoji_sequence(["lizard_1f98e.png","microbe.png","anatomical_heart.png","anatomical_heart.png"],downsample=DOWNSAMPLE)
     data_filename = "li_mi_an"
 
 
@@ -62,14 +62,14 @@ if data_index == 7:
 
 
 if nca_type_index==0:
-    schedule = optax.exponential_decay(2e-3, transition_steps=iters, decay_rate=0.99)
+    schedule = optax.exponential_decay(1e-3, transition_steps=iters, decay_rate=0.99)
     optimiser = optax.chain(optax.scale_by_param_block_norm(),
-                            optax.nadamw(schedule))
+                            optax.nadam(schedule))
     
     nca = NCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],KERNEL_SCALE=1,FIRE_RATE=0.5,PADDING="REPLICATE")
     opt = NCA_Trainer(nca,
                       data,
-                      model_filename="demo_2_emoji_nca_"+data_filename,
+                      model_filename="demo_stable_emoji_nca_"+data_filename,
                       DATA_AUGMENTER=data_augmenter_subclass,
                       GRAD_LOSS=True)
         
@@ -79,13 +79,13 @@ if nca_type_index==0:
             optimiser=optimiser,
             LOSS_FUNC_STR="euclidean")
 elif nca_type_index==1:
-    schedule = optax.exponential_decay(2e-3, transition_steps=iters, decay_rate=0.99)
+    schedule = optax.exponential_decay(1e-3, transition_steps=iters, decay_rate=0.99)
     optimiser = optax.chain(optax.scale_by_param_block_norm(),
-                            optax.nadamw(schedule))
+                            optax.nadam(schedule))
     nca = gNCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],KERNEL_SCALE=1,FIRE_RATE=0.5,PADDING="REPLICATE")
     opt = NCA_Trainer(nca,
                       data,
-                      model_filename="demo_2_emoji_gated_nca_"+data_filename,
+                      model_filename="demo_stable_emoji_gated_nca_"+data_filename,
                       DATA_AUGMENTER=data_augmenter_subclass,
                       GRAD_LOSS=True)
         
@@ -95,22 +95,22 @@ elif nca_type_index==1:
             optimiser=optimiser,
             LOSS_FUNC_STR="euclidean")
 
-elif nca_type_index==2:
-    schedule = optax.exponential_decay(2e-3, transition_steps=iters//10, decay_rate=0.99)
-    optimiser = optax.chain(optax.scale_by_param_block_norm(),
-                            optax.nadamw(schedule))
-    nca = kaNCA(6,KERNEL_STR=["ID","LAP","DIFF"],KERNEL_SCALE=1,FIRE_RATE=0.5,PADDING="REPLICATE")
-    opt = NCA_Trainer(nca,
-                      data,
-                      model_filename="demo_2_emoji_ka_nca_"+data_filename,
-                      DATA_AUGMENTER=data_augmenter_subclass,
-                      GRAD_LOSS=True)
+# elif nca_type_index==2:
+#     schedule = optax.exponential_decay(2e-3, transition_steps=iters//10, decay_rate=0.99)
+#     optimiser = optax.chain(optax.scale_by_param_block_norm(),
+#                             optax.nadamw(schedule))
+#     nca = kaNCA(6,KERNEL_STR=["ID","LAP","DIFF"],KERNEL_SCALE=1,FIRE_RATE=0.5,PADDING="REPLICATE")
+#     opt = NCA_Trainer(nca,
+#                       data,
+#                       model_filename="demo_2_emoji_ka_nca_"+data_filename,
+#                       DATA_AUGMENTER=data_augmenter_subclass,
+#                       GRAD_LOSS=True)
         
-    opt.train(t,
-            iters//10,
-            WARMUP=10,
-            optimiser=optimiser,
-            LOSS_FUNC_STR="euclidean")
+#     opt.train(t,
+#             iters//10,
+#             WARMUP=10,
+#             optimiser=optimiser,
+#             LOSS_FUNC_STR="euclidean")
 
     
 
