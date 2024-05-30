4a2d71971860248051bb96f61d29ce73d7b4ac96
Alex <alexander.richardson@gmail.com>
2024-05-30 15:09:22 +0100

    try training to the nice image morphing nca demos

--- train_pde_to_nca.py
+++ train_pde_to_nca.py
@@ -7,30 +7,69 @@ from PDE.model.reaction_diffusion_chemotaxis.update import F
 from PDE.model.solver.semidiscrete_solver import PDE_solver
 from PDE.trainer.PDE_trajectory_trainer import PDE_Trainer
 from NCA.model.NCA_model import NCA
+from NCA.model.NCA_gated_model import gNCA
 from Common.utils import load_emoji_sequence
+from Common.eddie_indexer import index_to_data_nca_type
 from NCA.trainer.data_augmenter_nca import DataAugmenter as DataAugmenterNCA
 import time
+import sys
 
 key = jax.random.PRNGKey(int(time.time()))
 
 
-
-
-CHANNELS = 16
+CHANNELS = 32
 CELL_CHANNELS = 3
 SIGNAL_CHANNELS = CHANNELS-CELL_CHANNELS
-
+index=int(sys.argv[1])-1
+data_index,nca_type_index = index_to_data_nca_type(index)
 # Load saved NCA model and data, then run to generate trajectory to train PDE to
-data = load_emoji_sequence(["alien_monster.png","microbe.png","rooster_1f413.png","rooster_1f413.png"],downsample=2)
+
+if data_index == 0:
+    data = load_emoji_sequence(["crab.png","microbe.png","alien_monster.png","alien_monster.png"],downsample=DOWNSAMPLE)
+    data_filename = "cr_mi_al"
+if data_index == 1:
+    data = load_emoji_sequence(["microbe.png","avocado_1f951.png","alien_monster.png","alien_monster.png"],downsample=DOWNSAMPLE)
+    data_filename = "mi_av_al"
+if data_index == 2:
+    data = load_emoji_sequence(["avocado_1f951.png","mushroom_1f344.png","lizard_1f98e.png","lizard_1f98e.png"],downsample=DOWNSAMPLE)
+    data_filename = "av_mu_li"
+if data_index == 3:
+    data = load_emoji_sequence(["mushroom_1f344.png","alien_monster.png","rooster_1f413.png","rooster_1f413.png"],downsample=DOWNSAMPLE)
+    data_filename = "mu_al_ro"
+
+
+#data = load_emoji_sequence(["alien_monster.png","microbe.png","rooster_1f413.png","rooster_1f413.png"],downsample=2)
 da = DataAugmenterNCA(data,12)
 da.data_init()
 x0 = np.array(da.split_x_y()[0])[0,0]
-nca = NCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],FIRE_RATE=1.0,PERIODIC=False)
-nca = nca.load("models/model_exploration/emoji_16_channels_64_sampling_relu_activation_ID_DIFF_LAP_kernels_v4.eqx")
+
+
+
+if nca_type_index==0:
+    nca = NCA(CHANNELS,KERNEL_STR=["ID","LAP","GRAD"],KERNEL_SCALE=1,PADDING="REPLICATE")
+    nca = nca.load("models/demo_stable_emoji_anisotropic_nca_"+data_filename)
+    nca_type="_pure_"
+elif nca_type_index==1:
+    nca = gNCA(CHANNELS,KERNEL_STR=["ID","LAP","GRAD"],KERNEL_SCALE=1,PADDING="REPLICATE")
+    nca = nca.load("models/demo_stable_emoji_anisotropic_gated_nca_"+data_filename)
+    nca_type="_gated_"
+elif nca_type_index==2:
+    nca = NCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],KERNEL_SCALE=1,PADDING="REPLICATE")
+    nca = nca.load("models/demo_stable_emoji_isotropic_nca_"+data_filename)
+    nca_type="_pure_"
+elif nca_type_index==3:
+    nca = gNCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],KERNEL_SCALE=1,PADDING="REPLICATE")
+    nca = nca.load("models/demo_stable_emoji_isotropic_gated_nca_"+data_filename)
+    nca_type="_gated_"
+#nca = NCA(CHANNELS,KERNEL_STR=["ID","LAP","DIFF"],FIRE_RATE=1.0,PERIODIC=False)
+#nca = nca.load("models/model_exploration/emoji_16_channels_64_sampling_relu_activation_ID_DIFF_LAP_kernels_v4.eqx")
+
+
+
 
 
 NCA_trajectory = nca.run(128,x0)
-NCA_trajectory = repeat(NCA_trajectory,"T C X Y -> B T C X Y",B=1)
+NCA_trajectory = repeat(NCA_trajectory,"T C X Y -> B T C X Y",B=4)
 print(NCA_trajectory.shape)
 
 
@@ -43,8 +82,8 @@ pde = PDE_solver(func,dt=0.1)
 
 
 # Define optimiser and lr schedule
-iters = 100
-schedule = optax.exponential_decay(1e-3, transition_steps=iters, decay_rate=0.99)
+iters = 1000
+schedule = optax.exponential_decay(1e-4, transition_steps=iters, decay_rate=0.99)
 #self.OPTIMISER = optax.adam(schedule)
 opt = non_negative_diffusion_chemotaxis(schedule)
 
@@ -52,6 +91,6 @@ opt = non_negative_diffusion_chemotaxis(schedule)
 
 trainer = PDE_Trainer(pde,
                       NCA_trajectory,
-                      model_filename="pde_chemreacdiff_to_emoji_nca_4")
+                      model_filename="pde_chemreacdiff_to_emoji"+nca_type+"nca_"+data_filename)
 
 trainer.train(32,iters,optimiser=opt)
