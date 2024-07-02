eae575bdfad419336a1962847bcb12c4dfb377ed
Alex <alexander.richardson@gmail.com>
2024-07-02 13:10:15 +0100

    move bash scripts to Experiments

--- micropattern_shape.array.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#$ -N mp_shape
-#$ -M s1605376@ed.ac.uk
-#$ -cwd
-#$ -l h_rt=24:00:00
-
-#$ -q gpu -l gpu=1 -pe sharedmem 4 -l h_vmem=80G
-
-bash micropattern_shape.sh $SGE_TASK_ID
--- micropattern_shape_mixed.array.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#$ -N mp_shape_mix
-#$ -M s1605376@ed.ac.uk
-#$ -cwd
-#$ -l h_rt=24:00:00
-
-#$ -q gpu -l gpu=1 -pe sharedmem 4 -l h_vmem=80G
-
-bash micropattern_shape_mixed.sh $SGE_TASK_ID
