bce89e5a0d40f03095e6da594e8e11ebbf3c33a6
Tao Li <s2009765@login03.ecdf.ed.ac.uk>
2024-07-12 09:15:34 +0100

    added directory for storing eddie output

---
+++ demo/demo.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# Grid Engine options (lines prefixed with #$)
+#$ -N gray              
+#$ -cwd
+#$ -e ../Eddie_OP
+#$ -o ../Eddie_OP                  
+#$ -l h_rt=00:05:00 
+#$ -l h_vmem=4G
+#  These options are:
+#  job name: -N
+#  use the current working directory: -cwd
+#  runtime limit of 5 minutes: -l h_rt
+#  memory limit of 1 Gbyte: -l h_vmem
+
+# Initialise the environment modules
+. /etc/profile.d/modules.sh
+
+# Load Anaconda
+module load anaconda/2024.02
+
+#Activate the environment
+source activate jax
+
+# Run the program
+python ./gray_scott_pde_demo.py
+conda deactivate
--- tests/test.py
+++
@@ -1,6 +0,0 @@
-import jax 
-
-
-a = jax.numpy.zeros((32,100,100))
-
-print(jax.nn.glu(a,axis=0).shape)
