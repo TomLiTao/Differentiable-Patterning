5dab024a646f518ae4cba1be7876d3fd22ccdf0f
Alex <alexander.richardson@gmail.com>
2024-07-22 11:37:40 +0100

    updating reaction-diffusion-advection

---
+++ PDE/model/reaction_diffusion_advection/reaction.py
@@ -0,0 +1,134 @@
+import jax
+import equinox as eqx
+import jax.numpy as jnp
+import time
+from jaxtyping import Array, Float
+
+
+class R(eqx.Module):
+    production_layers: list
+    decay_layers: list
+    N_CHANNELS: int
+    STABILITY_FACTOR: float
+    
+    def __init__(self,
+                 N_CHANNELS,
+                 INTERNAL_ACTIVATION,
+                 OUTER_ACTIVATION,
+                 INIT_SCALE,
+                 USE_BIAS,
+                 STABILITY_FACTOR,
+                 ZERO_INIT=True,
+                 key=jax.random.PRNGKey(int(time.time()))):
+        self.STABILITY_FACTOR = STABILITY_FACTOR
+        keys = jax.random.split(key,8)
+        self.N_CHANNELS = N_CHANNELS
+        self.production_layers = [
+            eqx.nn.Conv2d(
+                in_channels=self.N_CHANNELS,
+                out_channels=self.N_CHANNELS,
+                kernel_size=1,
+                padding=0,
+                use_bias=USE_BIAS,
+                key=keys[0]
+            ),
+            INTERNAL_ACTIVATION,
+            eqx.nn.Conv2d(
+                in_channels=self.N_CHANNELS,
+                out_channels=self.N_CHANNELS,
+                kernel_size=1,
+                padding=0,
+                use_bias=USE_BIAS,
+                key=keys[1]
+            ),
+            OUTER_ACTIVATION
+        ]
+        self.decay_layers = [
+            eqx.nn.Conv2d(
+                in_channels=self.N_CHANNELS,
+                out_channels=self.N_CHANNELS,
+                kernel_size=1,
+                padding=0,
+                use_bias=USE_BIAS,
+                key=keys[2]
+            ),
+            INTERNAL_ACTIVATION,
+            eqx.nn.Conv2d(
+                in_channels=self.N_CHANNELS,
+                out_channels=self.N_CHANNELS,
+                kernel_size=1,
+                padding=0,
+                use_bias=USE_BIAS,
+                key=keys[3]
+            ),
+            OUTER_ACTIVATION
+        ]
+        where = lambda l:l.weight
+        self.decay_layers[0] = eqx.tree_at(where,
+                                            self.decay_layers[0],
+                                            INIT_SCALE*jax.random.normal(key=keys[0],shape=self.decay_layers[0].weight.shape))
+        self.production_layers[0] = eqx.tree_at(where,
+                                            self.production_layers[0],
+                                            INIT_SCALE*jax.random.normal(key=keys[1],shape=self.production_layers[0].weight.shape))
+
+        self.decay_layers[-2] = eqx.tree_at(where,
+                                            self.decay_layers[-2],
+                                            INIT_SCALE*jax.random.normal(key=keys[2],shape=self.decay_layers[-2].weight.shape))
+        self.production_layers[-2] = eqx.tree_at(where,
+                                            self.production_layers[-2],
+                                            INIT_SCALE*jax.random.normal(key=keys[3],shape=self.production_layers[-2].weight.shape))
+        
+
+        if USE_BIAS:
+            where_b = lambda l:l.bias
+            self.decay_layers[0] = eqx.tree_at(where_b,
+                                                self.decay_layers[0],
+                                                INIT_SCALE*jax.random.normal(key=keys[4],shape=self.decay_layers[0].bias.shape))
+            self.production_layers[0] = eqx.tree_at(where_b,
+                                                self.production_layers[0],
+                                                INIT_SCALE*jax.random.normal(key=keys[5],shape=self.production_layers[0].bias.shape))
+
+            self.decay_layers[-2] = eqx.tree_at(where_b,
+                                                self.decay_layers[-2],
+                                                INIT_SCALE*jax.random.normal(key=keys[6],shape=self.decay_layers[-2].bias.shape))
+            self.production_layers[-2] = eqx.tree_at(where_b,
+                                                self.production_layers[-2],
+                                                INIT_SCALE*jax.random.normal(key=keys[7],shape=self.production_layers[-2].bias.shape))
+
+
+        if ZERO_INIT:
+            w_where = lambda l: l.weight
+            self.production_layers[-2] = eqx.tree_at(w_where,
+                                                     self.production_layers[-2],
+                                                     jnp.zeros(self.production_layers[-2].weight.shape))
+            self.decay_layers[-2] = eqx.tree_at(w_where,
+                                                self.decay_layers[-2],
+                                                jnp.zeros(self.decay_layers[-2].weight.shape))
+            if USE_BIAS:
+                b_where = lambda l: l.bias
+                self.production_layers[-2] = eqx.tree_at(b_where,
+                                                         self.production_layers[-2],
+                                                         jnp.zeros(self.production_layers[-2].bias.shape))
+                self.decay_layers[-2] = eqx.tree_at(b_where,
+                                                    self.decay_layers[-2],
+                                                    jnp.zeros(self.decay_layers[-2].bias.shape))
+
+
+
+    def __call__(self,X: Float[Array, "{self.N_CHANNELS} x y"])->Float[Array,"{self.N_CHANNELS} x y"]:
+        production = X
+        decay = X
+        for L in self.production_layers:
+            production = L(production)
+        
+        for L in self.decay_layers:
+            decay = L(decay)
+        
+        return production - X*decay - self.STABILITY_FACTOR*X**3#signals*jnp.max(jnp.abs(signals))*self.STABILITY_FACTOR#self.STABILITY_FACTOR*signals
+    
+    def partition(self):
+        return eqx.partition(self,eqx.is_array)
+    
+    def combine(self,diff,static):
+        self = eqx.combine(diff,static)
+        
---
+++ PDE/model/reaction_diffusion_advection/update.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 14:46:20 2023
+
+@author: s1605376
+"""
+import jax
+import equinox as eqx
+import jax.numpy as jnp
+import time
+from PDE.model.reaction_diffusion_advection.advection import V
+from PDE.model.reaction_diffusion_advection.reaction import R
+from PDE.model.reaction_diffusion_advection.diffusion import D
+from jaxtyping import Array, Float, PyTree, Scalar
+
+class F(eqx.Module):
+	f_v: V
+	f_r: R
+	f_d: D
+	N_CHANNELS: int
+	PADDING: str
+	dx: float
+	def __init__(self,
+			  	 N_CHANNELS,
+				 PADDING,
+				 dx,
+				 INTERNAL_ACTIVATION=jax.nn.relu,
+				 OUTER_ACTIVATION=jax.nn.tanh,
+				 INIT_SCALE=0.01,
+				 USE_BIAS=True,
+				 STABILITY_FACTOR=0.01,
+				 ZERO_INIT=True,
+				 key=jax.random.PRNGKey(int(time.time()))):
+		self.N_CHANNELS = N_CHANNELS
+		self.PADDING = PADDING
+		self.dx = dx
+		key1,key2,key3 = jax.random.split(key,3)
+
+		self.f_r = R(N_CHANNELS=N_CHANNELS,
+			   		 INTERNAL_ACTIVATION=INTERNAL_ACTIVATION,
+					 OUTER_ACTIVATION=OUTER_ACTIVATION,
+					 INIT_SCALE=INIT_SCALE,
+					 USE_BIAS=USE_BIAS,
+					 STABILITY_FACTOR=STABILITY_FACTOR,
+					 ZERO_INIT=ZERO_INIT,
+					 key=key1)
+		self.f_v = V(N_CHANNELS=N_CHANNELS,
+			   		 PADDING=PADDING,
+					 dx=dx,
+			   		 INTERNAL_ACTIVATION=INTERNAL_ACTIVATION,
+			   		 OUTER_ACTIVATION=OUTER_ACTIVATION,
+					 INIT_SCALE=INIT_SCALE,
+					 USE_BIAS=USE_BIAS,
+					 ZERO_INIT=ZERO_INIT,
+					 DIM = 2,																
+			   		 key=key2)
+		self.f_d = D(N_CHANNELS=N_CHANNELS,
+			   		 PADDING=PADDING,
+					 dx=dx,
+					 key=key3)
+
+	@eqx.filter_jit
+	def __call__(self,
+			  	 t: Float[Scalar, ""],
+				 X: Float[Array, "{self.N_CHANNELS} x y"],
+				 args)-> Float[Array, "{self.N_CHANNELS} x y"]:
+		"""
+
+		Parameters
+		----------
+		t : float32
+			timestep
+		
+		X : float32 [N_CHANNELS,_,_]
+			input PDE lattice state.
+		
+		args : None
+			Required for format of diffrax.ODETerm
+
+		Returns
+		-------
+		X_update : float32 [N_CHANNELS,_,_]
+			update to PDE lattice state state.
+
+		"""
+		return self.f_d(X) - self.f_v(X) + self.f_r(X)
+	
+	def partition(self):
+		r_diff,r_static = self.f_r.partition()
+		v_diff,v_static = self.f_v.partition()
+		d_diff,d_static = self.f_d.partition()
+		total_diff,total_static = eqx.partition(self,eqx.is_array)
+		#print(total_diff)
+		#print(total_static)
+		where_r = lambda m:m.f_r
+		where_v = lambda m:m.f_v
+		where_d = lambda m:m.f_d
+		
+		total_diff = eqx.tree_at(where_r,total_diff,r_diff)
+		total_diff = eqx.tree_at(where_v,total_diff,v_diff)
+		total_diff = eqx.tree_at(where_d,total_diff,d_diff)
+		
+		total_static = eqx.tree_at(where_r,total_static,r_static)
+		total_static = eqx.tree_at(where_v,total_static,v_static)
+		total_static = eqx.tree_at(where_d,total_static,d_static)
+		return total_diff,total_static
+	
+	def combine(self,diff,static):
+		self = eqx.combine(diff,static)
