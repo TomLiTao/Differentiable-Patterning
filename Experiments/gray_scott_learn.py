edfc46634b21ed9ad7dbb88935bc3b93ddd6dd33
Alex <alexander.richardson@gmail.com>
2024-08-30 12:26:34 +0100

    using euler solver for PDEs

--- Experiments/gray_scott_learn.py
+++ Experiments/gray_scott_learn.py
@@ -8,7 +8,7 @@ from PDE.trainer.optimiser import non_negative_diffusion
 from PDE.trainer.optimiser import multi_learnrate
 from einops import repeat
 from PDE.model.reaction_diffusion.update import F
-from PDE.model.solver.semidiscrete_solver import PDE_solver
+from PDE.model.solver.semidiscrete_solver_euler import PDE_solver
 from PDE.trainer.PDE_trainer import PDE_Trainer
 from PDE.model.fixed_models.update_gray_scott import F as F_gray_scott
 from PDE.model.fixed_models.update_chhabra import F as F_chhabra
@@ -37,7 +37,7 @@ SIZE = 64
 BATCHES = 8
 PADDING = "CIRCULAR"
 TRAJECTORY_LENGTH = PARAMS["TRAJECTORY_LENGTH"]
-
+dt = 1.0
 
 PDE_STR = "gray_scott"
 x0 = jr.uniform(key,shape=(BATCHES,2,SIZE,SIZE))
@@ -57,7 +57,7 @@ x0 = x0.at[:,0].set(1-x0[:,1])
 
 func = F_gray_scott(PADDING=PADDING,dx=1.0,KERNEL_SCALE=1)
 v_func = eqx.filter_vmap(func,in_axes=(None,0,None),out_axes=0)
-solver = PDE_solver(v_func,dt=0.01)
+solver = PDE_solver(v_func,dt=dt)
 T,Y = solver(ts=np.linspace(0,2000,PARAMS["TIME_RESOLUTION"]),y0=x0)
 Y = rearrange(Y,"T B C X Y -> B T C X Y")
 #Y = Y[:,:,:1] # Only include main channel, not inhibitor/other chemical
@@ -73,7 +73,7 @@ func = F(CHANNELS,
          dx=1.0,
          INTERNAL_ACTIVATION=PARAMS["INTERNAL_ACTIVATIONS"],
          #ADVECTION_OUTER_ACTIVATION=jax.nn.tanh,
-         INIT_SCALE={"reaction":0.5,"diffusion":0.5},
+         INIT_SCALE={"reaction":0.1,"diffusion":0.1},
          INIT_TYPE={"reaction":PARAMS["REACTION_INIT"],"diffusion":PARAMS["DIFFUSION_INIT"]},
          STABILITY_FACTOR=STABILITY_FACTOR,
          USE_BIAS=True,
@@ -81,7 +81,7 @@ func = F(CHANNELS,
          N_LAYERS=PARAMS["N_LAYERS"],
          ZERO_INIT={"reaction":False,"diffusion":False},
          key=key)
-pde = PDE_solver(func,dt=0.1)
+pde = PDE_solver(func,dt=dt)
 
 # Define optimiser and lr schedule
 #iters = 2000
@@ -89,7 +89,7 @@ pde = PDE_solver(func,dt=0.1)
 #opt = non_negative_diffusion(schedule,optimiser=OPTIMISER)
 #opt = optax.chain(optax.scale_by_param_block_norm(),
 			#PARAMS["OPTIMISER"](schedule))
-schedule = optax.exponential_decay(5e-4, transition_steps=ITERS, decay_rate=0.99)
+schedule = optax.exponential_decay(1e-4, transition_steps=ITERS, decay_rate=0.99)
 opt = multi_learnrate(
     schedule,
     rate_ratios={"advection": PARAMS["ADVECTION_RATIO"],
@@ -103,7 +103,7 @@ trainer = PDE_Trainer(PDE_solver=pde,
                       data=Y,
                       Ts=T,
                       #model_filename="pde_hyperparameters_chemreacdiff_emoji_anisotropic_nca_2/init_scale_"+str(INIT_SCALE)+"_stability_factor_"+str(STABILITY_FACTOR)+"act_"+INTERNAL_TEXT+"_"+OUTER_TEXT)
-                      model_filename="pde_hyperparameters_reacdiff_gray_scott/ch_"+str(CHANNELS)+"_tl_"+str(PARAMS["TRAJECTORY_LENGTH"])+"_resolution_"+str(PARAMS["TIME_RESOLUTION"])+"_ord_"+str(PARAMS["ORDER"])+"_layers_"+str(PARAMS["N_LAYERS"])+"_act_"+PARAMS["INTERNAL_ACTIVATIONS_TEXT"]+"_R_"+PARAMS["REACTION_INIT"]+PARAMS["REACTION_ZERO_INIT_TEXT"]+"_lrr_"+PARAMS["REACTION_RATIO_TEXT"]+"_D_"+PARAMS["DIFFUSION_INIT"]+PARAMS["DIFFUSION_ZERO_INIT_TEXT"]+"_opt_"+PARAMS["OPTIMISER_TEXT"])
+                      model_filename="pde_hyperparameters_reacdiff_gray_scott_euler/ch_"+str(CHANNELS)+"_tl_"+str(PARAMS["TRAJECTORY_LENGTH"])+"_resolution_"+str(PARAMS["TIME_RESOLUTION"])+"_ord_"+str(PARAMS["ORDER"])+"_layers_"+str(PARAMS["N_LAYERS"])+"_act_"+PARAMS["INTERNAL_ACTIVATIONS_TEXT"]+"_R_"+PARAMS["REACTION_INIT"]+PARAMS["REACTION_ZERO_INIT_TEXT"]+"_lrr_"+PARAMS["REACTION_RATIO_TEXT"]+"_D_"+PARAMS["DIFFUSION_INIT"]+PARAMS["DIFFUSION_ZERO_INIT_TEXT"]+"_opt_"+PARAMS["OPTIMISER_TEXT"])
 
 UPDATE_X0_PARAMS = {"iters":16,
                     "update_every":10000,
---
+++ PDE/model/solver/semidiscrete_solver_euler.py
@@ -0,0 +1,40 @@
+import jax
+import equinox as eqx
+import time
+import diffrax
+#from PDE.model.reaction_diffusion_advection.update import F
+
+from Common.model.abstract_model import AbstractModel
+
+class PDE_solver(AbstractModel):
+	func: eqx.Module	
+	dt0: float
+	rtol: float
+	atol: float
+	def __init__(self,F,dt=0.1,rtol=1e-3,atol=1e-3):
+		self.func = F
+		self.dt0 = dt
+		self.rtol=rtol
+		self.atol=atol
+	def __call__(self, ts, y0):
+		solution = diffrax.diffeqsolve(diffrax.ODETerm(self.func),
+									   diffrax.Euler(),
+									   t0=ts[0],t1=ts[-1],
+									   dt0=self.dt0,
+									   y0=y0,
+									   max_steps=1000*ts.shape[0],
+									   stepsize_controller=diffrax.ConstantStepSize(),
+									   #stepsize_controller=diffrax.PIDController(rtol=self.rtol, atol=self.atol),
+									   saveat=diffrax.SaveAt(ts=ts))
+		return solution.ts,solution.ys
+	
+	def partition(self):
+		func_diff,func_static = self.func.partition()
+		where = lambda s:s.func
+		total_diff,total_static = eqx.partition(self,eqx.is_array)
+		total_diff = eqx.tree_at(where,total_diff,func_diff)
+		total_static=eqx.tree_at(where,total_static,func_static)
+		return total_diff,total_static
+		
+	#def set_weights(self,weights):
+	#	self.func.set_weights(weights)
